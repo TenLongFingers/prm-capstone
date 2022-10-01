eb8e88f4675d24c2aaa4865aa30889d0a851480c
Chris McKnight <tenlongfingers10@gmail.com>
2022-10-01 17:44:20 -0600

    setting up the outline. Figuring out the folders

---
+++ src/Components/ComponentBrowser/landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+//landing page components
+import signInBox from "..comp/landingpage/signInBox";
+
+export default class ComponentBrowser extends Component {
+  render() {
+    return;
+    <div className="signInContainer">
+      <signInBox />
+    </div>;
+  }
+}
---
+++ src/Components/ComponentBrowser/landingpage/signInBox.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class signInBox extends Component {
+  render() {
+    return <p>Sign In Box here</p>;
+  }
+}
+
+export default signInBox;
